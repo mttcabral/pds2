67b6a35d841d13fcd3597652a5162696535e04ba
Mateus Cabral <mateusmacabral@gmail.com>
2025-04-08 21:20:52 -0300

    Add searchBus method to Company.cpp

--- VPL3/Company.cpp
+++ VPL3/Company.cpp
@@ -8,3 +8,16 @@ Company::Company()
         bus[i] = nullptr;
     }
 }
+
+Bus *Company::searchBus(std::string licensePlate)
+{
+    for (int i = 0; i < 20; i++)
+    {
+        if (bus[i]->licensePlate == licensePlate)
+        {
+            return bus[i];
+        }
+    }
+
+    return nullptr;
+}
