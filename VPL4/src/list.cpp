cd9ed8b4bb6d003e022e5655d781a430861fd58c
Mateus Cabral <mateusmacabral@gmail.com>
2025-04-14 21:58:55 -0300

    Add initial code given in class

---
+++ VPL4/src/list.cpp
@@ -0,0 +1,29 @@
+#include "list.hpp"
+
+#include <iostream>
+
+void List::insertNode(int data) {
+    Node* aux = new Node;
+    aux->data = data;
+    aux->next = nullptr;
+    if (head == nullptr) {
+        head = aux;
+        tail = aux;
+    } else {
+        tail->next = aux;
+        tail = aux;
+    }
+}
+
+void List::display() {
+    Node* aux = head;
+    while (aux != nullptr) {
+        cout << aux->data << " ";
+        aux = aux->next;
+    }
+    cout << endl;
+}
+
+// Você deverá implementar apenas esse método
+void List::inverte() {
+}
